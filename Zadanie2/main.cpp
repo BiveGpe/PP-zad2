c55ab027874f063d01bd5371884b7a0c6a42f28d
BiveGpe <89411621+BiveGpe@users.noreply.github.com>
2022-10-22 14:53:09 +0200

    Add files via upload

---
+++ Zadanie2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int howManyPeople = 5;
+    int howManyPizza = 23;
+
+    int result = howManyPizza / howManyPeople;
+
+    cout << result;
+
+    return 0;
+}
